e177c583c492a03fe8285f3e43ebbf89005a8411
Lewis Porter <porter.lewis@gmail.com>
2019-08-27 15:06:33 -0400

    matplotlib is shit

--- graphing.py
+++ graphing.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 import csvlogging
-import time
+from time import strftime, localtime
 import csv
 
 CSVFILE = 'csvlog.csv'
@@ -11,12 +11,25 @@ def returnListFromCSV(CSVFILE):
         newList = list(csv.reader(csvFile))
     return newList
 
+def shortTime(time):
+    time = float(time)
+    try:
+        shortened_time = strftime("%a %H", localtime(time))
+    except ValueError:
+        pass
+    return shortened_time
+
 DATA_LIST = returnListFromCSV(CSVFILE)
 
-datetime_x = [row[0] for row in DATA_LIST][1:]
-temp_y  = [row[2] for row in DATA_LIST][1:]
-humid_y  = [row[3] for row in DATA_LIST][1:]
+datetime_x = [shortTime(row[1]) for row in DATA_LIST[1:]]
+standard_y = [n for n in range(10,100, 5)]
+temp_y  = [row[2] for row in DATA_LIST[1:]]
+humid_y  = [row[3] for row in DATA_LIST[1:]]
 
-plt.plot(datetime_x, temp_y, humid_y)
+plt.plot(datetime_x, temp_y)
+plt.plot(datetime_x, humid_y)
+plt.grid(True)
+plt.axis(tight=True,auto=True)
 
-plt.savefig('graph.png')
+
+plt.savefig('graph.png', dpi=150)
